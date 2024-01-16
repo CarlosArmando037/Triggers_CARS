674de0fa07133adf58b39c87e0711239b72e5233
Perla118 <perlamaria9545@gmail.com>
2024-01-16 11:10:21 -0700

    first commit

---
+++ CARS_Triggers/BotonTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CARS_Triggers
+{
+    public class BotonTrigger : TriggerAction<Button>
+    {
+        protected override void Invoke(Button btn)
+        {
+            btn.BackgroundColor = Color.DarkOrange;
+            btn.Text = "Here we go!!!";
+        }
+    }
+}
---
+++ CARS_Triggers/Deslizadorcolor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CARS_Triggers
+{
+    internal class Deslizadorcolor : TriggerAction<Slider>
+    {
+        protected override void Invoke(Slider sender)
+        {
+            var value = sender.Value;
+
+            if (value < 40)
+                sender.BackgroundColor = Color.Red;
+            else if (value > 60)
+                sender.BackgroundColor = Color.Green;
+            else
+                sender.BackgroundColor = Color.Black;
+        }
+    }
+}
